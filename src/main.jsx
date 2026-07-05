81fd875ba1e703f852219b7012bc91b7f39549ef
Olle <olle.marsliden@gmail.com>
2026-07-05 19:46:58 +0200

    Lägg till komplett Vite-projekt

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import VaderApp from "./Vader.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VaderApp />
+  </React.StrictMode>
+);
